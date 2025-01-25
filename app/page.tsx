a1063937e30a1d47e88902c665c5f8aad417a77e
Montassar TOUATI <montassar.t@winsoftinformatique.tn>
2025-01-25 12:00:30 +0100

    corection

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/Header";
 import Commerciale from "@/components/solutions/Commerciale";
-import Garage from "@/components/solutions/Garage";
 
 export default function Home() {
   return (
